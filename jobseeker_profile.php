8723f741f6ee4b8aa9b826d8d7b944e0359ebcfc
Hemlata Bhundhoo <hemlata.b@hotmail.co.uk>
2019-07-05 23:50:24 +0400

    Refactor employer profile

--- jobseeker_profile.php
+++ jobseeker_profile.php
@@ -5,21 +5,15 @@
 	<link rel="stylesheet" type="text/css" href="css/styles.css">
 	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 	<link href='https://fonts.googleapis.com/css?family=Days One' rel='stylesheet'>
-	<style type="text/css">
-		.error{
-
-			color: red;
-		}
-	</style>
 </head>
 <body>
 
-	<nav>
+	<nav class="navbar">
 		<ul>
 			<li><a href="index.php">Home</a></li>
 			<li><a href="contact.php">Contact</a></li>
-			<li style="float: right, background-color:orange"><a  href="logout.php">Logout</a></li>
-			<li style="float: right"><a class="active" href="#">Dashboard</a></li>
+			<li><a  href="logout.php">Logout</a></li>
+			<li><a class="active" href="#">Dashboard</a></li>
 		</ul>
 	</nav>
 			
@@ -40,17 +34,17 @@
 
 			<div class="details">
 
-				<h3 style="color:#e25b89; font-weight:bold;">Hemlata Bhundhoo</h3>
+				<h3> Hemlata Bhundhoo</h3>
 
 				<h4>Junior Developer</h4>
 
 				<hr id="hr-profile">
 
-				<i class="fa fa-map-marker" aria-hidden="true"></i>London 
+				<i class="fa fa-map-marker" aria-hidden="true"></i> London 
 
-				<i class="fa fa-phone" aria-hidden="true"></i>441234567898  
+				<i class="fa fa-phone" aria-hidden="true"></i> 441234567898  
 
-				<i class="fa fa-envelope-o" aria-hidden="true"></i>hemlata@gmail.com
+				<i class="fa fa-envelope-o" aria-hidden="true"></i> hemlata@gmail.com
 		</div>
 
 		</div>
@@ -63,9 +57,9 @@
 
 			<div class="left-container">
 
-				<i class="fa fa-file-text-o" aria-hidden="true"></i><span style="font-family: 'Days One';">CV</span>
+				<i class="fa fa-file-text-o" aria-hidden="true"></i><span> CV</span>
 				<hr>
-				<h3 style=" font-family: 'Days One';">Manage your CV</h3>
+				<h3 class="manage-cv">Manage your CV</h3>
 
 				<p>Upload a new CV, or use our CV builder to build one for you.</p>
 				<button id="upload-cv">Upload CV</button>
@@ -74,7 +68,7 @@
 
 		<div class="right-container">
 
-			<span style=" font-family: 'Days One';">CV Visibility<i class="fa fa-binoculars" aria-hidden="true"></i></span>
+			<span> CV Visibility <i class="fa fa-binoculars" aria-hidden="true"></i> </span>
 			<hr>
 			<p>Let recruiters know you are looking for job</p>
 		</div>
@@ -85,7 +79,7 @@
 
 			<div class="left-container" id="cover-letter">
 
-				<i class="fa fa-file-text-o" aria-hidden="true"></i><span style="font-family: 'Days One';">Cover letter</span>
+				<i class="fa fa-file-text-o" aria-hidden="true"></i><span> Cover letter</span>
 				<hr>
 				<textarea cols="70" rows="20" id="text-letter">
 					
@@ -97,7 +91,7 @@
 
 		<div class="right-container">
 
-			<i class="fa fa-spinner" aria-hidden="true"></i><span style="font-family: 'Days One';">Recommended jobs</span>
+			<i class="fa fa-spinner" aria-hidden="true"></i><span> Recommended jobs</span>
 			<hr>
 			<a href="#">junior php</a><br>
 			<a href="#">junior php</a><br>
@@ -109,14 +103,14 @@
 
 	</div>
 
-	<div class="footer">	
-		
+	<div class="footer">
+		<div class="social">
 		<a href="#" class="fa fa-facebook"></a>
 		<a href="#" class="fa fa-twitter"></a>
 		<a href="#" class="fa fa-google"></a>
 		<a href="#" class="fa fa-linkedin"></a>
 	
-		<p> <small>Copyright &copy; 2017</small></p>
+		<p> <small>Copyright &copy; <?php echo date('Y');?></small></p>
 
 	</div>
 
